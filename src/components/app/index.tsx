3a4fc405da721307f0c1034d088408fcac43abd0
hattrick <dasha.khitrick@gmail.com>
2024-07-13 00:48:39 +0300

    init: init project with configuration

---
+++ src/components/app/index.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div>
+            <h1>dfdef</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ webpack.common.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            cacheDirectory: true,
+                        },
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            esModule: true,
+                            modules: {
+                                namedExport: true,
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+}
