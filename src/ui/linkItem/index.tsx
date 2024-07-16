ca54a9544580e7aa51534eed97fad0eea70ed94b
hattrick <dasha.khitrick@gmail.com>
2024-07-16 18:24:34 +0300

    feat: add ui components (switcher, logo, linkItem)

---
+++ src/ui/linkItem/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+type PropsType = {
+    path: string
+    title: string
+    className: string
+    onClick: () => void
+}
+
+function LinkItem({ path, title, className, onClick }: PropsType) {
+    return (
+        <li key={path} className={className}>
+            <Link to={path} onClick={onClick}>
+                {title}
+            </Link>
+        </li>
+    )
+}
+
+LinkItem.propTypes = {
+    path: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    className: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+}
+
+export default React.memo(LinkItem)
