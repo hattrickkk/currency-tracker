79103c7545bdcfde9f104f49cb28d080ec0cd5f1
hattrick <dasha.khitrick@gmail.com>
2024-07-22 01:32:06 +0300

    refactor cardsContainer component

--- src/components/cardsContainer/index.tsx
+++ src/components/cardsContainer/index.tsx
@@ -6,41 +6,39 @@ import currencies from '@mockData/currency'
 import CurrencyCard from '@ui/currencyCard'
 
 import * as styles from './style.module.scss'
+import CURRENCIES_SECTION_NAMES from '@constants/currenciesSectionNames'
 
 type Props = {
     elements: (string | Index)[]
-    type: 'currency' | 'index'
+    type: CURRENCIES_SECTION_NAMES
 }
 
 function CardsContainer({ elements, type }: Props) {
     const { setCurrentCurrency, openPopup } = useContext(popupContext)
 
+    const currecyCardClickHandler = (el: string) => () => {
+        setCurrentCurrency({
+            code: currencies.data[el].code,
+            name: currencies.data[el].name,
+        })
+        openPopup()
+    }
+
     return (
         <div className={styles.cardsContainer}>
-            {elements.map((el: string | Index) => {
-                return type === 'currency' ? (
-                    <CurrencyCard
-                        key={currencies.data[el as string].code}
-                        name={currencies.data[el as string].name}
-                        code={currencies.data[el as string].code}
-                        picture={CURRENCIES_SYMBOLS[el as string]}
-                        onClick={() => {
-                            setCurrentCurrency({
-                                code: currencies.data[el as string].code,
-                                name: currencies.data[el as string].name,
-                            })
-                            openPopup()
-                        }}
-                    />
-                ) : (
-                    <CurrencyCard
-                        key={(el as Index).name}
-                        name={(el as Index).name}
-                        value={(el as Index).value}
-                        picture={(el as Index).picture}
-                    />
-                )
-            })}
+            {type === CURRENCIES_SECTION_NAMES.STOCKS
+                ? elements.map(({ name, value, picture }: Index) => (
+                      <CurrencyCard key={name} name={name} value={value} picture={picture} />
+                  ))
+                : elements.map((el: string) => (
+                      <CurrencyCard
+                          key={currencies.data[el].code}
+                          name={currencies.data[el].name}
+                          code={currencies.data[el].code}
+                          picture={CURRENCIES_SYMBOLS[el]}
+                          onClick={currecyCardClickHandler(el)}
+                      />
+                  ))}
         </div>
     )
 }
--- src/components/currenciesSection/index.tsx
+++ src/components/currenciesSection/index.tsx
@@ -6,15 +6,16 @@ import Title from '@ui/title'
 
 import * as global from '@styles/global.module.scss'
 import * as styles from './style.module.scss'
+import CURRENCIES_SECTION_NAMES from '@constants/currenciesSectionNames'
 
 function CurrenciesSection() {
     return (
         <section className={styles.currencies}>
             <div className={global.container}>
-                <Title value='Stocks' />
-                <CardsContainer elements={INDEXES} type='index' />
-                <Title value='Quotes' />
-                <CardsContainer elements={Object.keys(currencies.data)} type='currency' />
+                <Title value={CURRENCIES_SECTION_NAMES.STOCKS} />
+                <CardsContainer elements={INDEXES} type={CURRENCIES_SECTION_NAMES.STOCKS} />
+                <Title value={CURRENCIES_SECTION_NAMES.QUOTES} />
+                <CardsContainer elements={Object.keys(currencies.data)} type={CURRENCIES_SECTION_NAMES.QUOTES} />
             </div>
         </section>
     )
