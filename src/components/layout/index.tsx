7bf14686ac2986a31f08fb776d0c4215ec2d6b08
hattrick <dasha.khitrick@gmail.com>
2024-07-25 18:01:26 +0300

    faet: add layout component

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,31 @@
+import { Suspense } from 'react'
+import { Outlet } from 'react-router-dom'
+import Banner from '@components/banner'
+import ErrorBoundary from '@components/errorBoundary'
+import Footer from '@components/footer'
+import Header from '@components/header'
+import LastUpdated from '@ui/lastUpdated'
+
+import * as styles from './styles.module.scss'
+
+function Layout() {
+    return (
+        <div className={styles.wrapper}>
+            <ErrorBoundary>
+                <>
+                    <Header />
+                    <Banner />
+                    <LastUpdated time='2024-07-10T23:59:59Z' />
+                    <main className={styles.main}>
+                        <Suspense>
+                            <Outlet />
+                        </Suspense>
+                    </main>
+                    <Footer />
+                </>
+            </ErrorBoundary>
+        </div>
+    )
+}
+
+export default Layout
