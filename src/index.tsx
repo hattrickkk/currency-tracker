3b628f87f7f4b485369973998702f5f916db437a
hattrick <dasha.khitrick@gmail.com>
2024-07-29 14:03:42 +0300

    feat: add combinedProvider

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from '@components/app'
-import CurrencyContextProvider from '@components/currencyContextProvider'
-import PopupContextProvider from '@components/popupContextProvider'
-import ThemeContextProvider from '@components/themeContextProvider'
+import CombinedProvider from '@components/compinedProvider'
 
 import '@styles/nullStyle.scss'
 
@@ -12,14 +10,10 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
     <React.StrictMode>
-        <ThemeContextProvider>
-            <CurrencyContextProvider>
-                <PopupContextProvider>
-                    <BrowserRouter>
-                        <App />
-                    </BrowserRouter>
-                </PopupContextProvider>
-            </CurrencyContextProvider>
-        </ThemeContextProvider>
+        <CombinedProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </CombinedProvider>
     </React.StrictMode>
 )
